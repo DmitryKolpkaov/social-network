b63e9ff3110119a0b523903b18745cd2fb220c80
Dmitriy Kolpakov <111364017+DmitryKolpkaov@users.noreply.github.com>
2023-06-04 14:17:25 +0600

    Миграция таблицы users

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->bigIncrements('id');
+            $table->string('email');
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('location')->nullable();
+            $table->rememberToken()->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
